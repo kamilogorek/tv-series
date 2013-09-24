3131b4a1a6d302f74b08a32b5196fe381cedfbd5
kamilogorek <kamil.ogorek@gmail.com>
2013-09-24 22:52:10 +0200

    Updated list with my own data and added todos

--- data/proposals.js
+++ data/proposals.js
@@ -1,9 +1,53 @@
-// Add your proposal to the list, but please, 
+// Add your proposal to the list, but please,
 // remember about alphabetical order. If your show is
 // already on the list, just put your github account
 // in the 'votes' part. Thanks!
 
 var toConsider = [{
+  "title": "According to Jim",
+  "imdb": "http://www.imdb.com/title/tt0285351/",
+  "episodes": 182,
+  "totalEpisodes": null,
+  "episodeLength": 25,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Alcatraz",
+  "imdb": "http://www.imdb.com/title/tt1728102",
+  "episodes": 13,
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "American Dad",
+  "imdb": "http://www.imdb.com/title/tt0397306",
+  "episodes": 152,
+  "episodeLength": 21,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "American Horror Story",
+  "imdb": "http://www.imdb.com/title/tt1844624",
+  "episodes": "12",
+  "totalEpisodes": "25",
+  "episodeLength": 45,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Anger Management",
+  "imdb": "http://www.imdb.com/title/tt1986770",
+  "episodes": 37,
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Arrow",
+  "imdb": "http://www.imdb.com/title/tt2193021/",
+  "episodes": 23,
+  "episodeLength": 44,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
   "title": "Band of Brothers",
   "imdb": "http://www.imdb.com/title/tt0185906",
   "episodes": "10",
@@ -11,12 +55,34 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["tobeytailor"]
 }, {
+  "title": "Banshee",
+  "imdb": "http://www.imdb.com/title/tt2017109/",
+  "episodes": "11",
+  "episodeLength": 55,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
   "title": "Black Mirror",
   "imdb": "http://www.imdb.com/title/tt2085059/",
   "episodes": "6",
   "episodeLength": 60,
   "stillWatching": 0,
   "votes": ["tizianozonta"]
+}, {
+  "title": "Boston Public",
+  "imdb": "http://www.imdb.com/title/tt0247081/",
+  "episodes": "81",
+  "episodeLength": 44,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Boardwalk Empire",
+  "imdb": "http://www.imdb.com/title/tt0979432/",
+  "episodes": "12",
+  "totalEpisodes": 36,
+  "episodeLength": 60,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
 }, {
   "title": "Burn Notice",
   "imdb": "http://www.imdb.com/title/tt0810788/",
@@ -32,6 +98,21 @@ var toConsider = [{
   "stillWatching": 1,
   "votes": ["kamilogorek", "sirgregg"]
 }, {
+  "title": "Cleveland Show",
+  "imdb": "http://www.imdb.com/title/tt1195935/",
+  "episodes": "65",
+  "totalEpisodes": "75",
+  "episodeLength": 23,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Common Law",
+  "imdb": "http://www.imdb.com/title/tt1771072/",
+  "episodes": "12",
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
   "title": "Community",
   "imdb": "http://www.imdb.com/title/tt1439629/",
   "episodes": 84,
@@ -46,12 +127,91 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["datrio", "kamilogorek"]
 }, {
+  "title": "CSI: Las vegas",
+  "imdb": "http://www.imdb.com/title/tt0247082/",
+  "episodes": "121",
+  "totalEpisodes": "287",
+  "episodeLength": 45,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Czterej Pancerni i Pies",
+  "imdb": "http://www.imdb.com/title/tt0120948/",
+  "episodes": "21",
+  "episodeLength": 55,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Da Vinci's Demons",
+  "imdb": "http://www.imdb.com/title/tt2094262/",
+  "episodes": 8,
+  "episodeLength": 60,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
   "title": "Doctor Who",
   "imdb": "http://www.imdb.com/title/tt0436992/",
   "episodes": 105,
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["sirgregg"]
+}, {
+  "title": "Elementary",
+  "imdb": "http://www.imdb.com/title/tt2191671/",
+  "episodes": 24,
+  "episodeLength": 44,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Family Guy",
+  "imdb": "http://www.imdb.com/title/tt0182576/",
+  "episodes": 210,
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Flashforward",
+  "imdb": "http://www.imdb.com/title/tt1441135/",
+  "episodes": "22",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Franklin & Bash",
+  "imdb": "http://www.imdb.com/title/tt1600199/",
+  "episodes": 30,
+  "episodeLength": 42,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Fringe",
+  "imdb": "http://www.imdb.com/title/tt1119644/",
+  "episodes": "100",
+  "episodeLength": 47,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Futurama",
+  "imdb": "http://www.imdb.com/title/tt0149460/",
+  "episodes": 131,
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Hannibal",
+  "imdb": "http://www.imdb.com/title/tt2243973",
+  "episodes": 13,
+  "episodeLength": 43,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Heroes",
+  "imdb": "http://www.imdb.com/title/tt0813715",
+  "episodes": "59",
+  "totalEpisodes": "77",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
 }, {
   "title": "Homeland",
   "imdb": "http://www.imdb.com/tt1796960",
@@ -60,6 +220,34 @@ var toConsider = [{
   "stillWatching": 1,
   "votes": ["tobeytailor", "shorang"]
 }, {
+  "title": "House MD",
+  "imdb": "http://www.imdb.com/title/tt0412142/",
+  "episodes": "177",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "House of Cards",
+  "imdb": "http://www.imdb.com/title/tt1856010",
+  "episodes": 13,
+  "episodeLength": 60,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "I, Claudius",
+  "imdb": "http://www.imdb.com/title/tt0074006/",
+  "episodes": 12,
+  "episodeLength": 54,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "IT Crowd",
+  "imdb": "http://www.imdb.com/title/tt0487831",
+  "episodes": "24",
+  "episodeLength": 25,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
   "title": "It's Always Sunny in Philadelphia",
   "imdb": "http://www.imdb.com/title/tt0472954/",
   "episodes": 94,
@@ -67,12 +255,77 @@ var toConsider = [{
   "stillWatching": 1,
   "votes": ["sirgregg"]
 }, {
+  "title": "Jericho",
+  "imdb": "http://www.imdb.com/title/tt0805663",
+  "episodes": "29",
+  "episodeLength": 43,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Joey",
+  "imdb": "http://www.imdb.com/title/tt0375355/",
+  "episodes": 11,
+  "episodeLength": 24,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "King of Queens",
+  "imdb": "http://www.imdb.com/title/tt0165581/",
+  "episodes": 207,
+  "episodeLength": 22,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Kyle XY",
+  "imdb": "http://www.imdb.com/title/tt0756509/",
+  "episodes": "10",
+  "totalEpisodes": "43",
+  "episodeLength": 44,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Last Resort",
+  "imdb": "http://www.imdb.com/title/tt2172103/",
+  "episodes": "13",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Lie to Me",
+  "imdb": "http://www.imdb.com/title/tt1235099/",
+  "episodes": "48",
+  "episodeLength": 42,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Louie",
+  "imdb": "http://www.imdb.com/title/tt1492966",
+  "episodes": 39,
+  "episodeLength": 23,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "MacGuyver",
+  "imdb": "http://www.imdb.com/title/tt0088559/",
+  "episodes": 64,
+  "totalEpisodes": "139",
+  "episodeLength": 48,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
   "title": "Mad Men",
   "imdb": "http://www.imdb.com/title/tt0804503",
   "episodes": "65",
   "episodeLength": 45,
   "stillWatching": 1,
   "votes": ["kamilogorek", "datrio"]
+}, {
+  "title": "Mentalist",
+  "imdb": "http://www.imdb.com/title/tt1196946",
+  "episodes": 114,
+  "episodeLength": 42,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
 }, {
   "title": "Modern Family",
   "imdb": "http://www.imdb.com/title/tt1442437/",
@@ -88,12 +341,12 @@ var toConsider = [{
   "stillWatching": 1,
   "votes": ["dos1"]
 }, {
-  "title": "My Name Is Earl",
-  "imdb": "http://www.imdb.com/title/tt0460091",
-  "episodes": "96",
-  "episodeLength": 30,
+  "title": "My wife and kids",
+  "imdb": "http://www.imdb.com/title/tt0273855",
+  "episodes": "122",
+  "episodeLength": 23,
   "stillWatching": 0,
-  "votes": ["kamilogorek", "sirgregg"]
+  "votes": ["michalbe"]
 }, {
   "title": "Numb3rs",
   "imdb": "http://www.imdb.com/title/tt0433309/",
@@ -102,19 +355,19 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["afronski"]
 }, {
+  "title": "Odwroceni",
+  "imdb": "http://www.imdb.com/title/tt0998810",
+  "episodes": "13",
+  "episodeLength": 45,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
   "title": "Oz",
   "imdb": "http://www.imdb.com/title/tt0118421",
   "episodes": "56",
   "episodeLength": 55,
   "stillWatching": 0,
   "votes": ["kamilogorek"]
-}, {
-  "title": "Parks and Recreations",
-  "imdb": "http://www.imdb.com/title/tt1266020",
-  "episodes": "83",
-  "episodeLength": 21,
-  "stillWatching": 1,
-  "votes": ["kamilogorek", "sirgregg"]
 }, {
   "title": "Person of Interest",
   "imdb": "http://www.imdb.com/title/tt1839578",
@@ -123,19 +376,41 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["shorang"]
 }, {
-  "title": "Pitbull",
-  "imdb": "http://www.imdb.com/title/tt1096980",
-  "episodes": "31",
-  "episodeLength": 45,
-  "stillWatching": 0,
-  "votes": ["kamilogorek", "afronski"]
-}, {
-  "title": "Sons of Anarchy",
-  "imdb": "http://www.imdb.com/title/tt1124373",
-  "episodes": "56",
-  "episodeLength": 45,
+  "title": "Revenge",
+  "imdb": "http://www.imdb.com/title/tt1837642/",
+  "episodes": 44,
+  "episodeLength": 44,
   "stillWatching": 1,
-  "votes": ["kamilogorek", "tobeytailor","tizianozonta"]
+  "votes": ["michalbe"]
+}, {
+  "title": "Scrubs",
+  "imdb": "http://www.imdb.com/title/tt0285403",
+  "episodes": "182",
+  "episodeLength": 23,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Sex and the City",
+  "imdb": "http://www.imdb.com/title/tt0159206",
+  "episodes": "94",
+  "episodeLength": 30,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Sherlock",
+  "imdb": "http://www.imdb.com/title/tt1475582",
+  "episodes": "6",
+  "episodeLength": 90,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "Simpsons, The",
+  "imdb": "http://www.imdb.com/title/tt0096697/",
+  "episodes": 23,
+  "totalEpisodes": "523",
+  "episodeLength": 24,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
 }, {
   "title": "Spartacus",
   "imdb": "http://www.imdb.com/title/tt1442449",
@@ -143,6 +418,28 @@ var toConsider = [{
   "episodeLength": 60,
   "stillWatching": 1,
   "votes": ["kamilogorek", "datrio", "afronski", "tobeytailor"]
+}, {
+  "title": "Stawka większa niż życie",
+  "imdb": "http://www.imdb.com/title/tt0065035/",
+  "episodes": "18",
+  "episodeLength": 60,
+  "stillWatching": 0,
+  "votes": ["michalbe"]
+}, {
+  "title": "Suits",
+  "imdb": "http://www.imdb.com/title/tt1632701/",
+  "episodes": 34,
+  "episodeLength": 42,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
+  "title": "That '70s Show",
+  "imdb": "http://www.imdb.com/title/tt0165598/",
+  "episodes": 53,
+  "totalEpisodes": 200,
+  "episodeLength": 22,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
 }, {
   "title": "The Following",
   "imdb": "http://www.imdb.com/title/tt2071645",
@@ -157,13 +454,6 @@ var toConsider = [{
   "episodeLength": 60,
   "stillWatching": 1,
   "votes": ["tobeytailor"]
-}, {
-  "title": "The Office",
-  "imdb": "http://www.imdb.com/title/tt0386676",
-  "episodes": "179",
-  "episodeLength": 22,
-  "stillWatching": 0,
-  "votes": ["kamilogorek"]
 }, {
   "title": "The Pacific",
   "imdb": "http://www.imdb.com/tt0374463",
@@ -200,6 +490,13 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["datrio"]
 }, {
+  "title": "Two and a half men",
+  "imdb": "http://www.imdb.com/title/tt0369179",
+  "episodes": 223,
+  "episodeLength": 21,
+  "stillWatching": 1,
+  "votes": ["michalbe"]
+}, {
   "title": "Weeds",
   "imdb": "http://www.imdb.com/title/tt0439100/",
   "episodes": "102",
@@ -207,10 +504,10 @@ var toConsider = [{
   "stillWatching": 0,
   "votes": ["tizianozonta"]
 }, {
-  "title": "Wilfred",
-  "imdb": "http://www.imdb.com/title/tt1703925",
-  "episodes": "26",
-  "episodeLength": 30,
+  "title": "White Collar",
+  "imdb": "http://www.imdb.com/title/tt1358522/",
+  "episodes": 61,
+  "episodeLength": 42,
   "stillWatching": 1,
-  "votes": ["kamilogorek", "sirgregg"]
-}]
+  "votes": ["michalbe"]
+}];
--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 ## UPDATE 27.03.2013 ##
-The list is now dynamically rendered based on data JSON files. You can find it here: [Tv Shows](http://michalbe.github.com/tv-series/).
+The list is now dynamically rendered based on data JSON files. You can find it here: [TV Shows](http://kamilogorek.pl/tv-series/).
 
 ## My TV Shows List
-This is the list of all the TV series I've been watching since February 2008. I probably forget about some of them, so I'll update this list in near future. In the meantime - help me find some new interesting titles! If you want to propose something, just fork this repo and send pull request with the list updated in [TV Shows to consider](https://github.com/michalbe/tv-series/blob/master/data/proposals.js) part. Thanks in advance!
+This is the list of all the TV series I've been watching since forever. I probably forget about some of them, so I'll update this list in near future. In the meantime - help me find some new interesting titles! If you want to propose something, just fork this repo and send pull request with the list updated in [TV Shows to consider](https://github.com/kamilogorek/tv-series/blob/master/data/proposals.js) part. Thanks in advance!
+
+## TODO:
+[ ] Add animes
+[ ] Move "The" prexies to the end of the title
+[ ] Add notes column
+[ ] Correct titles lettering
+[ ] Review imdb shows list and add those already watched
